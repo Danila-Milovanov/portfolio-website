0ea2adb0277d9e10724e52b702ee0132d0b23e8e
Danila-Milovanov <9019853@student.zadkine.nl>
2025-11-07 13:48:18 +0100

    finally fixed

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -2,17 +2,23 @@ import { Link, Outlet } from "react-router-dom";
 
 function Navbar() {
   return (
-    <>
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+    <nav className="bg-gray-800 p-4 text-white">
+      <ul className="flex justify-center space-x-8">
+        <li>
+          <Link to="/" className="hover:text-yellow-400">Home</Link>
+        </li>
+        <li>
+          <Link to="/projects" className="hover:text-yellow-400">Projects</Link>
+        </li>
+        <li>
+          <Link to="/about" className="hover:text-yellow-400">About</Link>
+        </li>
+        <li>
+          <Link to="/contact" className="hover:text-yellow-400">Contact</Link>
+        </li>
       </ul>
+      <Outlet />
     </nav>
-    <Outlet/>
-    </>
   );
 }
 
